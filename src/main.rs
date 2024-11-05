a635de3b51ce28ec5cfb279b13dc85072e106dc6
Dawid <dawid.malecki21@gmail.com>
2024-11-05 22:48:27 +0100

    check adapton

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+#[macro_use] extern crate adapton;
 mod graph;
 mod tsp;
 mod tsp_draw;
 mod tsp_comp;
 
 fn main() {
-    tsp_draw::draw();
+    // tsp_draw::draw();
+
+    tsp_comp::run();
 }
--- src/tsp_comp.rs
+++ src/tsp_comp.rs
@@ -1,128 +1,62 @@
-use std::{
-    collections::{HashSet, VecDeque},
-    hash::{Hash, Hasher},
-    rc::Rc,
-};
+use adapton::macros::*;
+use adapton::engine::*;
 
-use depends::{
-    derives::{Dependencies, Operation, Value},
-    error::EarlyExit,
-    Dependency, DerivedNode, InputNode, Resolve, SingleRef, TargetMut, UpdateDerived, UpdateInput,
-};
+fn generate() {
+    let edges = [10, 20, 30, 40, 50, 60];
 
+    let inputs = edges.map(|x| {
+        let cell = cell!(x);
+        cell
+    });
 
-// #[derive(Value, Default, Hash)]
-// struct Vertex {
-//     id: i32,
-// }
+    let mut outputs = Vec::from(inputs.clone());
 
-// impl Vertex {
-//     fn new(id: i32) -> Vertex {
-//         Vertex { id }
-//     }
-// }
+    while outputs.len() > 1 {
+        let mut new_inputs = vec![];
 
-// impl UpdateInput for Vertex {
-//     type Update = i32;
+        print!("Outputs: {}", outputs.len());
 
-//     fn update_mut(&mut self, update: Self::Update) {
-//         self.id = update;
-//     }
-// }
-
-#[derive(Value, Default, Hash)]
-pub struct PathLength {
-    pub value: i32
-}
-
-impl PathLength {
-    fn new(value: i32) -> PathLength {
-        PathLength { value }
-    }
-}
-
-impl UpdateInput for PathLength {
-    type Update = i32;
-
-    fn update_mut(&mut self, update: Self::Update) {
-        self.value = update;
-    }
-}
-
-#[derive(Dependencies)]
-struct TwoPaths {
-    lhs: PathLength,
-    rhs: PathLength,
-}
-
-#[derive(Operation)]
-struct Add;
-
-impl UpdateDerived for Add {
-    type Input<'a> = TwoPathsRef<'a> where Self: 'a;
-    type Target<'a> = TargetMut<'a, PathLength> where Self: 'a;
-
-    fn update_derived(input: Self::Input<'_>, mut target: Self::Target<'_>) -> Result<(), EarlyExit> {
-        let TwoPathsRef { lhs, rhs } = input;
-        target.value = lhs.value + rhs.value;
-        Ok(())
-    }
-}
-
-enum NodeWrapper {
-    First(Rc<DerivedNode<TwoPathsDep<InputNode<PathLength>, InputNode<PathLength>>, Add, PathLength>>),
-    Second(Rc<DerivedNode<TwoPathsDep<
-        DerivedNode<TwoPathsDep<InputNode<PathLength>, InputNode<PathLength>>, Add, PathLength>, 
-        DerivedNode<TwoPathsDep<InputNode<PathLength>, InputNode<PathLength>>, Add, PathLength>
-    >, Add, PathLength>>),
-    Third(Rc<DerivedNode<TwoPathsDep<
-        DerivedNode<TwoPathsDep<InputNode<PathLength>, InputNode<PathLength>>, Add, PathLength>, 
-        DerivedNode<TwoPathsDep<InputNode<PathLength>, InputNode<PathLength>>, Add, PathLength>
-    >, Add, PathLength>>),
-}
-
-struct TSPPathLengthIncrementalComputation {
-    input_nodes: Vec<Rc<InputNode<PathLength>>>,
-}
-
-impl TSPPathLengthIncrementalComputation {
-    fn new(edges: Vec<i32>) -> Self {
-        let mut input_nodes = Vec::new();
-        
-        edges.iter().for_each(|edge| {
-            let node = InputNode::new(PathLength::new(*edge));
-            input_nodes.push(node);
-        });
-
-        if input_nodes.len() % 2 == 1 {
-            let dummy = InputNode::new(PathLength::new(0));
-            input_nodes.push(dummy);
+        if outputs.len() % 2 == 1 {
+            outputs.push(cell!(0));
         }
 
-        let mut nodes = input_nodes
-            .chunks(2)
-            .map(|chunk| {
-                let lhs = Rc::clone(&chunk[0]);
-                let rhs = Rc::clone(&chunk[1]);
-                let derived = DerivedNode::new(
-                    TwoPaths::init(lhs, rhs),
-                    Add,
-                    PathLength::default()
-                );
-                NodeWrapper::Simple(derived)
-            }).collect::<VecDeque<NodeWrapper>>();
-        
-        // while nodes.len() > 1 {
-        //     let lhs = nodes.pop_front().unwrap();
-        //     let rhs = nodes.pop_front().unwrap();
-        //     let derived = DerivedNode::new(
-        //         TwoPaths::init(Rc::clone(&lhs), Rc::clone(&rhs)),
-        //         Add,
-        //         PathLength::default()
-        //     );
-        //     // nodes.push_back(derived);
-        // }
+        let mut i = 1;
+        while i < outputs.len() {
+            let a = outputs[i - 1].clone();
+            let b = outputs[i].clone();
 
-        TSPPathLengthIncrementalComputation { input_nodes }        
+            let c = thunk!(get!(a) + get!(b));
+            new_inputs.push(c.clone());
+
+            i += 2;
+        }
+
+    
+        outputs = new_inputs.clone();
     }
+
+    print!("Result: {}\n", get!(outputs[0]));
+}
+
+pub fn run() {
+    manage::init_dcg();
+
+    generate();
+
+    // let num = cell!(42);
+    // let den = cell!(2);
+
+    // // In Rust, cloning is explicit:
+    // let den2 = den.clone(); // clone _global reference_ to cell.
+    // let den3 = den.clone(); // clone _global reference_ to cell, again.
+
+    // // Two subcomputations: The division, and a check thunk with a conditional expression
+    // let div   = thunk![ get!(num) / get!(den) ];
+    // let check = thunk![ if get!(den2) == 0 { None } else { Some(get!(div)) } ];
+
+    // print!("num: {}\n", get!(check).unwrap());
+
+    // set(&den3, 4);
+
+    // print!("num: {}\n", get!(check).unwrap());
 }
